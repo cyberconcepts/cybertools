d171dddc0ea7d4bfa3db3675d13f9634a5410fc3
helmutm <helmutm@fd906abe-77d9-0310-91a1-e0d9ade77398>
2006-06-02 13:28:35 +0000

    Suppress admin menu
    git-svn-id: svn://svn.cy55.de/Zope3/src/cybertools/trunk@1238 fd906abe-77d9-0310-91a1-e0d9ade77398

---
+++ browser/loops/menu.py
@@ -0,0 +1,32 @@
+#
+#  Copyright (c) 2006 Helmut Merz helmutm@cy55.de
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+"""
+login, logout and similar stuff.
+
+$Id$
+"""
+
+from zope.app.publisher. browser import menu
+
+class MenuAccessView(menu.MenuAccessView):
+
+    def __getitem__(self, menuId):
+        if menuId in ('zmi_actions', 'help_actions'):
+            return []
+        return super(MenuAccessView, self).__getitem__(menuId)
