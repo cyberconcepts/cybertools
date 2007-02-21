b4dd7ccdddcbaac658711f62c4060e5f7d821bdf
helmutm <helmutm@fd906abe-77d9-0310-91a1-e0d9ade77398>
2007-02-21 20:14:13 +0000

    make skin definitionsconforming to Zope 3.3, i.e. get rid of some deprecation messages
    git-svn-id: svn://svn.cy55.de/Zope3/src/cybertools/trunk@1592 fd906abe-77d9-0310-91a1-e0d9ade77398

--- browser/liquid/__init__.py
+++ browser/liquid/__init__.py
@@ -3,7 +3,14 @@ $Id$
 """
 
 from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.app.rotterdam import Rotterdam
+
 
 class liquid(IBrowserRequest):
     """The `liquid` layer."""
+
+
+class Liquid(liquid, Rotterdam):
+    """ The Liquid skin """
 
--- browser/loops/__init__.py
+++ browser/loops/__init__.py
@@ -3,7 +3,9 @@ $Id$
 """
 
 from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.app.rotterdam import Rotterdam
 
-class liquid(IBrowserRequest):
-    """The `liquid` layer."""
 
+class Loops(Rotterdam):
+    """ The Loops skin """
