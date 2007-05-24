e78ecc2a3f5cbe644156488622fc2f3958216811
helmutm <helmutm@fd906abe-77d9-0310-91a1-e0d9ade77398>
2007-05-24 12:55:36 +0000

    more on i18n and German translations
    git-svn-id: svn://svn.cy55.de/Zope3/src/cybertools/trunk@1767 fd906abe-77d9-0310-91a1-e0d9ade77398

--- browser/loops/__init__.py
+++ browser/loops/__init__.py
@@ -5,7 +5,9 @@ $Id$
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.app.rotterdam import Rotterdam
+from cybertools.browser.liquid import Liquid
 
 
+#class Loops(Liquid):
 class Loops(Rotterdam):
     """ The Loops skin """
