dc7aa556c592b306a16312e86f0e28ea4a35e810
helmutm <helmutm@fd906abe-77d9-0310-91a1-e0d9ade77398>
2007-08-02 20:19:39 +0000

    new package pyscript, derived from zope.app.PythonPage
    git-svn-id: svn://svn.cy55.de/Zope3/src/cybertools/trunk@1867 fd906abe-77d9-0310-91a1-e0d9ade77398

---
+++ pyscript/interfaces.py
@@ -0,0 +1,57 @@
+#
+#  Copyright (c) 2007 Helmut Merz helmutm@cy55.de
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+"""
+interface definitions for the pyscript package.
+
+$Id$
+"""
+
+from zope.interface import Interface
+from zope import schema
+
+
+class IPythonPage(Interface):
+    """Python Page
+
+    The Python Page acts as a simple content type that allows you to execute
+    Python in content space. Additionally, if you have a free-standing
+    triple-quoted string, it gets converted to a print statement
+    automatically.
+    """
+
+    source = schema.SourceText(
+        title=_(u"Source"),
+        description=_(u"The source of the Python page."),
+        required=True,
+    )
+
+    contentType = schema.TextLine(
+        title=_(u"Content Type"),
+        description=_(u"The content type the script outputs."),
+        required=True,
+        default=u"text/html",
+    )
+
+    def __call__(request, **kw):
+        """Execute the script.
+
+        The script will insert the `request` and all `**kw` as global
+        variables. Furthermore, the variables `script` and `context` (which is
+        the container of the script) will be added.
+        """
---
+++ pyscript/script.py
@@ -0,0 +1,136 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Python Page
+
+$Id$
+"""
+
+__docformat__ = 'restructuredtext'
+
+import new
+import re
+from cStringIO import StringIO
+from persistent import Persistent
+from zope.proxy import removeAllProxies
+from zope.security.untrustedpython.builtins import SafeBuiltins
+from zope.security.untrustedpython.rcompile import compile
+from zope.traversing.api import getParent, getPath
+from zope.app.container.contained import Contained
+#from zope.app.interpreter.interfaces import IInterpreter
+from zope.interface import implements
+from zope.app.i18n import ZopeMessageFactory as _
+
+from cybertools.pyscript.interfaces import IPythonPage
+
+
+class PythonPage(Contained, Persistent):
+    """Persistent Python Page - Content Type
+    """
+
+    implements(IPythonPage)
+
+    _v_compiled = None
+
+    def __init__(self, source=u'', contentType=u'text/plain'):
+        """Initialize the object."""
+        super(PythonPage, self).__init__()
+        self.source = source
+        self.contentType = contentType
+
+    def __filename(self):
+        if self.__parent__ is None:
+            filename = 'N/A'
+        else:
+            filename = getPath(self)
+        return filename
+
+    def setSource(self, source):
+        """Set the source of the page and compile it.
+
+        This method can raise a syntax error, if the source is not valid.
+        """
+        self.__source = source
+        self.__prepared_source = self.prepareSource(source)
+        # Compile objects cannot be pickled
+        self._v_compiled = Function(self.__prepared_source, self.__filename())
+
+    _tripleQuotedString = re.compile(
+        r"^([ \t]*)[uU]?([rR]?)(('''|\"\"\")(.*)\4)", re.MULTILINE | re.DOTALL)
+
+    def prepareSource(self, source):
+        """Prepare source."""
+        # compile() don't accept '\r' altogether
+        source = source.replace("\r\n", "\n")
+        source = source.replace("\r", "\n")
+        if isinstance(source, unicode):
+            # Use special conversion function to work around
+            # compiler-module failure to handle unicode in literals
+            try:
+                source = source.encode('ascii')
+            except UnicodeEncodeError:
+                return self._tripleQuotedString.sub(_print_usrc, source)
+        return self._tripleQuotedString.sub(r"\1print u\2\3", source)
+
+
+    def getSource(self):
+        """Get the original source code."""
+        return self.__source
+
+    source = property(getSource, setSource)
+
+    def __call__(self, request, **kw):
+        output = StringIO()
+        if self._v_compiled is None:
+            self._v_compiled = Function(self.__prepared_source,
+                                               self.__filename())
+        kw['request'] = request
+        kw['script'] = self
+        kw['untrusted_output'] = output
+        kw['printed'] = output
+        kw['context'] = getParent(self)
+        kw['script_result'] = None
+        self._v_compiled(kw)
+        result = kw['script_result']
+        if result == output:
+            result = result.getvalue()
+        return result
+
+
+class Function(object):
+    """A compiled function.
+    """
+
+    def __init__(self, source, filename='<string>'):
+        lines = []
+        lines.insert(0, 'def dummy():')
+        for line in source.splitlines():
+            lines.append('    ' + line)
+        lines.append('script_result = dummy()')
+        source = '\n'.join(lines)
+        print source
+        self.code = compile(source, filename, 'exec')
+
+    def __call__(self, globals):
+        globals['__builtins__'] = SafeBuiltins
+        #fct = new.function(self.code, globals)
+        exec self.code in globals, None
+        #return fct()
+
+
+def _print_usrc(match):
+    string = match.group(3)
+    raw = match.group(2)
+    if raw:
+        return match.group(1)+'print '+`string`
+    return match.group(1)+'print u'+match.group(3).encode('unicode-escape')
