3dc405d3475d99f54c00ec8b3b49137c0aa0c9ca
Helmut Merz <helmutm@cy55.de>
2012-01-27 07:51:53 +0100

    work in progress: new package docgen for document generation

---
+++ docgen/tests.py
@@ -0,0 +1,26 @@
+"""
+unit tests, doc tests
+"""
+
+import unittest, doctest
+from zope.testing.doctestunit import DocFileSuite
+from zope.interface.verify import verifyClass
+from zope.interface import implements
+
+
+class Test(unittest.TestCase):
+    "Basic tests for the docgen package."
+
+    def testInterfaces(self):
+        pass
+
+
+def test_suite():
+    flags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
+    return unittest.TestSuite((
+                unittest.makeSuite(Test),
+                DocFileSuite('README.txt', optionflags=flags),
+            ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
