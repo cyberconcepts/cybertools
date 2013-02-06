2a60b121000b7871eb4a38b1a9b34b06fd94ec1e
Helmut Merz <helmutm@cy55.de>
2013-02-06 10:18:44 +0100

    minor fixes

--- relation/__init__.py
+++ relation/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 # -*- Mode: Python; py-indent-offset: 4 -*-
 #
-#  Copyright (c) 2005 Helmut Merz helmutm@cy55.de
+#  Copyright (c) 2013 Helmut Merz helmutm@cy55.de
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -21,8 +21,6 @@
 """
 The relation package provides all you need for setting up dyadic and
 triadic relations.
-
-$Id$
 """
 
 from persistent import Persistent
--- relation/registry.py
+++ relation/registry.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (c) 2011 Helmut Merz helmutm@cy55.de
+#  Copyright (c) 2013 Helmut Merz helmutm@cy55.de
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,8 +18,6 @@
 
 """
 Implementation of the utilities needed for the relations package.
-
-$Id$
 """
 
 from logging import getLogger
