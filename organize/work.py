65181d3098e38209b257072a79be08619944fc99
Helmut Merz <helmutm@cy55.de>
2016-02-20 21:30:21 +0100

    update editing rules (still only informative) to changed list of work item states

--- organize/work.py
+++ organize/work.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (c) 2014 Helmut Merz helmutm@cy55.de
+#  Copyright (c) 2016 Helmut Merz helmutm@cy55.de
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -111,21 +111,21 @@ fieldNames = ['title', 'description', 'deadline', 'priority', 'activity',
 #           . default (may be empty)
 
 editingRules = dict(
-    plan    = {'*':         '+++.....+'},
-    accept  = {'*':         '+++.....-',
-               'planned':   '+++++++.-',
-               'accepted':  '+++++++.-'},
-    start   = {'*':         '+++./...-'},
-    work    = {'*':         '+++.....-',
-               'running':   '++++....-'},
-    finish  = {'*':         '+++.....-',
-               'running':   '++++....-'},
-    cancel  = {'*':         '+++////./'},
-    modify  = {'*':         '+++++++++'},
-    delegate= {'*':         '+++++++.+'},
-    move    = {'*':         '+++++++.-'},
-    close   = {'*':         '+++////./'},
-    reopen  = {'*':         '+++////./'},
+    plan    = {'*':         '+++++.....+'},
+    accept  = {'*':         '+++++.....-',
+               'planned':   '+++++++++.-',
+               'accepted':  '+++++++++.-'},
+    start   = {'*':         '+++++./...-'},
+    work    = {'*':         '+++++.....-',
+               'running':   '++++++....-'},
+    finish  = {'*':         '+++++.....-',
+               'running':   '++++++....-'},
+    cancel  = {'*':         '+++++////./'},
+    modify  = {'*':         '+++++++++++'},
+    delegate= {'*':         '+++++++++.+'},
+    move    = {'*':         '+++++++++.-'},
+    close   = {'*':         '+++++////./'},
+    reopen  = {'*':         '+++++////./'},
 )
 
 
