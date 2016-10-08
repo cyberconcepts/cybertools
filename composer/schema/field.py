4c18a9731ccef1032449dc0de175317244ebf342
Helmut Merz <helmutm@cy55.de>
2016-10-08 11:17:39 +0200

    add 'visible' property to field definition

--- composer/schema/field.py
+++ composer/schema/field.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (c) 2014 Helmut Merz helmutm@cy55.de
+#  Copyright (c) 2016 Helmut Merz helmutm@cy55.de
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -54,6 +54,7 @@ class Field(Component):
 
     implements(IField)
 
+    visible = True
     required = False
     readonly = False
     nostore = False
