383b77edf14ad9c9078125d3e4346c456a67b5bd
Helmut Merz <helmutm@cy55.de>
2024-09-22 13:31:58 +0200

    knowledge: Python3 fixes

--- cybertools/knowledge/provider.py
+++ cybertools/knowledge/provider.py
@@ -1,35 +1,15 @@
-#
-#  Copyright (c) 2006 Helmut Merz helmutm@cy55.de
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+# cybertools.knowledge.provider
 
-"""
-Knowledge providers provide knowledge.
-
-$Id$
+""" Knowledge providers provide knowledge.
 """
 
-from zope.interface import implements
+from zope.interface import implementer
 from cybertools.knowledge.interfaces import IKnowledgeProvider
 
 
+@implementer(IKnowledgeProvider)
 class KnowledgeProvider(object):
 
-    implements(IKnowledgeProvider)
-    
     def __init__(self):
         self._providedKnowledge = {}
 
