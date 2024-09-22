b60d6d3b10c02c67a92d16d85708c82a9980ba60
Helmut Merz <helmutm@cy55.de>
2024-09-22 16:12:36 +0200

    external: some Python3 fixes

--- cybertools/external/element.py
+++ cybertools/external/element.py
@@ -1,33 +1,14 @@
-#
-#  Copyright (c) 2010 Helmut Merz helmutm@cy55.de
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+# cybertools.external.element
 
-"""
-Basic implementation of the elements used for the intermediate format for export
+""" Basic implementation of the elements used for the intermediate format for export
 and import of objects.
-
-$Id$
 """
 
 import os
 from zope import component
 from zope.cachedescriptors.property import Lazy
 from zope.dottedname.resolve import resolve
-from zope.interface import Interface, implements
+from zope.interface import Interface, implementer
 from zope.traversing.api import getName, traverse
 
 from cybertools.composer.interfaces import IInstance
@@ -35,9 +16,8 @@ from cybertools.composer.schema.interfaces import ISchemaFactory
 from cybertools.external.interfaces import IElement
 
 
+@implementer(IElement)
 class Element(dict):
-
-    implements(IElement)
 
     encoding = 'UTF-8'
     type = ''
