5f3bd3c04f0148e144f900ac0eb67e543c9dedfc
Helmut Merz <helmutm@cy55.de>
2024-09-24 11:14:22 +0200

    composer: more Python3 fixes

--- cybertools/composer/layout/region.py
+++ cybertools/composer/layout/region.py
@@ -1,36 +1,16 @@
-#
-#  Copyright (c) 2008 Helmut Merz helmutm@cy55.de
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+# cybertools.composer.layout.region
 
-"""
-Region implementation.
-
-$Id$
+""" Region implementation.
 """
 
-from zope.interface import implements
+from zope.interface import implementer
 
 from cybertools.composer.layout.interfaces import IRegion
 from cybertools.util.jeep import Jeep
 
 
+@implementer(IRegion)
 class Region(object):
-
-    implements(IRegion)
 
     allowedLayoutCategories = None
 
